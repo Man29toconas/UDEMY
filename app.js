81c3a4828a4951ddacce2c30e8f402b52b5cec51
Man29toconas <toconascruz@gmail.com>
2023-08-23 11:17:28 -0300

    actualizacion

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3030
 const app = express();
 
 app.use(express.static("public"))
